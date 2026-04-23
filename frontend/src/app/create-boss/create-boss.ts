7d24fb479590e1dab94d240f3e93a9def5e7e40b
Manuel Rabl <is251020@ustp-students.at>
2026-04-23 15:28:35 +0200

    admin panel design changes

--- frontend/src/app/create-boss/create-boss.ts
+++ frontend/src/app/create-boss/create-boss.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-boss',
@@ -6,4 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './create-boss.html',
   styleUrl: './create-boss.css',
 })
-export class CreateBoss {}
+export class CreateBoss {
+  imagePreview: string | ArrayBuffer | null = null;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) return;
+
+    const file = input.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+
+      // 🔥 wichtig
+      this.cdr.detectChanges();
+    };
+
+    reader.readAsDataURL(file);
+  }
+}
--- frontend/src/app/manager-panel/manager-panel.ts
+++ frontend/src/app/manager-panel/manager-panel.ts
@@ -2,6 +2,7 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 import { NgForOf, NgIf } from '@angular/common';
 import {CreateQuestions} from '../create-questions/create-questions';
 import {CreateBoss} from '../create-boss/create-boss';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manager-panel',
@@ -11,7 +12,14 @@ import {CreateBoss} from '../create-boss/create-boss';
 })
 export class ManagerPanel {
 
+  constructor(private router:Router) {
+  }
+
   protected window:number = 0;
+
+  goHome() {
+    this.router.navigate(['/home']);
+  }
 
 
 }
